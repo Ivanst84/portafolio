aff6c788131a893b9e372e8820631a500a0b5938
Ivanst84 <77261169+Ivanst84@users.noreply.github.com>
2023-05-04 21:27:44 -0700

    se cambio el menu con fonts  efectos de iconos de redes sociales

--- vista/proyecto/index.php
+++ vista/proyecto/index.php
@@ -1,32 +1,20 @@
 
 <?php require_once '../../controlador/proyecto.php'?>
-  <!-- otros elementos head -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-    crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
-    integrity="sha384-g2/ebR0raWWAvi1VKOcfn0mOSXWcS60Zo2Qxg0/lui01BRsW4UC4d4mJ0Fg1Uu9C"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-    crossorigin="anonymous"></script>
-
 
 <link rel="stylesheet" href="../../public/estilos.css">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+<?php require_once("../html/mainHead.php"); ?>
 
 <head>
-<?php require_once("../html/mainHeader.php"); ?>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
+
+  <?php require_once("../html/mainMenu.php"); ?>
+
 </head>
 
 <body>
-<?php require_once("../html/mainMenu.php"); ?>
 
 
 <div class="container">
